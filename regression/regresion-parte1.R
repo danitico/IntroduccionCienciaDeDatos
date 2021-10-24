7eb1353a5e20f2542d5f3b2b1c90190c38ae86b0
Daniel Ranchal Parrado <danitico98@gmail.com>
2021-10-24 17:56:13 +0200

    Regression almost done (#3)
    * Regression almost done
    
    * Finish regression
    
    * This part is finished

---
+++ regression/regresion-parte1.R
@@ -0,0 +1,102 @@
+source("utils.R")
+
+if (!requireNamespace("tidyverse")) {
+    install.packages("tidyverse")
+}
+if (!requireNamespace("ggplot2")) {
+    install.packages("ggplot2")
+}
+
+library(tidyverse)
+library(ggplot2)
+
+# tenemos que predecir MedianHouseValue
+df <- read.keel("california.dat") # Tarda un rato..
+df <- df %>% mutate(MedianHouseValue=as.numeric(MedianHouseValue))
+
+str(df)
+
+# Matriz de scatter plots
+pairs(df)
+
+fit1 <- lm(MedianHouseValue~Longitude, data=df)
+summary(fit1)
+
+fit2 <- lm(MedianHouseValue~Latitude, data=df)
+summary(fit2)
+
+fit3 <- lm(MedianHouseValue~HousingMedianAge, data=df)
+summary(fit3)
+
+fit4 <- lm(MedianHouseValue~TotalRooms, data=df)
+summary(fit4)
+
+fit5 <- lm(MedianHouseValue~TotalBedrooms, data=df)
+summary(fit5)
+
+fit6 <- lm(MedianHouseValue~Population, data=df)
+summary(fit6)
+
+fit7 <- lm(MedianHouseValue~Households, data=df)
+summary(fit7)
+
+fit8 <- lm(MedianHouseValue~MedianIncome, data=df)
+summary(fit8)
+
+# El mejor model con una sola variable independiente es aquel que utiliza MediaIncome, es decir, fit8
+
+rmsefit8 <- sqrt(sum(fit8$residuals^2)/(length(fit8$residuals)-2))
+rmsefit8
+
+# Predicción
+yprime <- predict(fit8, data.frame(MedianIncome=df$MedianIncome))
+
+# Plotting original relation between the two variables and the new model
+df %>% ggplot(
+    aes(x=MedianIncome, y=MedianHouseValue)
+) + geom_point(
+    alpha=0.3
+) + geom_line(
+    aes(
+        x=MedianIncome,
+        y=yprime
+    ),
+    color="blue"
+)
+
+
+# En este modelo añadimos todas las variables independientes
+# Este modelo es mucho mejor que cuando solo se utiliza MedianIncome como variable independiente
+multipleVariablesFit1 <- lm(MedianHouseValue~., data=df)
+summary(multipleVariablesFit1)
+
+
+# No linealidad
+
+# No mejora el modelo en el que se utilizan todas las variables. Mejora levemente
+# respecto al modelo fit8, en el que no se tiene en cuenta la variable al cuadrado
+noLinealityFit1 <- lm(MedianHouseValue~MedianIncome + I(MedianIncome^2), df)
+summary(noLinealityFit1)
+df %>% ggplot(
+    aes(x=MedianIncome, y=MedianHouseValue)
+) + geom_point(
+    alpha=0.3
+) + geom_line(
+    aes(
+        x=MedianIncome,
+        y=predict(noLinealityFit1, data.frame(MedianIncome=MedianIncome))
+    ),
+    color="blue"
+)
+
+# Interacciones
+
+interactionsFit1 <- lm(MedianHouseValue~TotalRooms*TotalBedrooms, data=df)
+summary(interactionsFit1)
+
+# El mejor modelo con interacciones
+interactionsFit2 <- lm(
+    MedianHouseValue~. + Households*TotalBedrooms + Households*MedianIncome + HousingMedianAge*MedianIncome,
+    data=df
+)
+summary(interactionsFit2)
---
+++ regression/regresion-parte2.R
@@ -0,0 +1,166 @@
+source("utils.R")
+
+if (!requireNamespace("tidyverse")) {
+    install.packages("tidyverse")
+}
+if (!requireNamespace("ggplot2")) {
+    install.packages("ggplot2")
+}
+if (!requireNamespace("kknn")) {
+    install.packages("kknn")
+}
+
+library(tidyverse)
+library(ggplot2)
+library(kknn)
+
+# Tenemos que predecir MedianHouseValue
+df <- read.keel("california.dat") # Tarda un rato..
+df <- df %>% mutate(MedianHouseValue=as.numeric(MedianHouseValue))
+
+# Knn consigue mejores resultados que una regresión lineal multiple
+# Con knn el rmse es 339.597 mientras que con la regresión lineal múltiple es de 613.7
+fitknn1 <- kknn(MedianHouseValue ~ ., df, df)
+yprime1 <- fitknn1$fitted.values
+
+# Gráfico mostrando los datos originales y la predicción del modelo
+df %>% ggplot(aes(x=MedianIncome, y=MedianHouseValue)) + geom_point() + geom_point(aes(x=MedianIncome, y=yprime1), color="blue")
+
+# Error cuadrático medio
+sqrt(sum((df$MedianHouseValue - yprime1)^2)/length(yprime1))
+
+
+# Otros tipos de modelos con otras variables
+
+fitknn2 <- kknn(MedianHouseValue ~ MedianIncome, df, df)
+yprime2 <- fitknn2$fitted.values
+sqrt(sum((df$MedianHouseValue - yprime2)^2)/length(yprime2))
+df %>% ggplot(aes(x=MedianIncome, y=MedianHouseValue)) + geom_point() + geom_point(aes(x=MedianIncome, y=yprime2), color="blue")
+
+fitknn3 <- kknn(MedianHouseValue~MedianIncome + I(MedianIncome^2), df, df)
+yprime3 <- fitknn3$fitted.values
+sqrt(sum((df$MedianHouseValue - yprime3)^2)/length(yprime3))
+df %>% ggplot(aes(x=MedianIncome, y=MedianHouseValue)) + geom_point() + geom_point(aes(x=MedianIncome, y=yprime3), color="blue")
+
+fitknn4 <- kknn(MedianHouseValue~TotalRooms*TotalBedrooms, df, df)
+yprime4 <- fitknn4$fitted.values
+sqrt(sum((df$MedianHouseValue - yprime4)^2)/length(yprime4))
+df %>% ggplot(aes(x=MedianIncome, y=MedianHouseValue)) + geom_point() + geom_point(aes(x=MedianIncome, y=yprime4), color="blue")
+
+fitknn5 <- kknn(MedianHouseValue~. + Households*TotalBedrooms + Households*MedianIncome + HousingMedianAge*MedianIncome, df, df)
+yprime5 <- fitknn5$fitted.values
+sqrt(sum((df$MedianHouseValue - yprime5)^2)/length(yprime5))
+df %>% ggplot(aes(x=MedianIncome, y=MedianHouseValue)) + geom_point() + geom_point(aes(x=MedianIncome, y=yprime5), color="blue")
+
+# Cross validation
+
+#------------- 5-fold cross-validation LM todas las variables
+nombre <- "california"
+run_lm_fold <- function(i, x, tt = "test") {
+    file <- paste(x, "-5-", i, "tra.dat", sep="")
+    x_tra <- read.csv(file, comment.char="@", header=FALSE)
+    file <- paste(x, "-5-", i, "tst.dat", sep="")
+    x_tst <- read.csv(file, comment.char="@", header=FALSE)
+    In <- length(names(x_tra)) - 1
+    names(x_tra)[1:In] <- paste ("X", 1:In, sep="")
+    names(x_tra)[In+1] <- "Y"
+    names(x_tst)[1:In] <- paste ("X", 1:In, sep="")
+    names(x_tst)[In+1] <- "Y"
+    if (tt == "train") {
+        test <- x_tra
+    }
+    else {
+        test <- x_tst
+    }
+    fitMulti=lm(Y~.,x_tra)
+    yprime=predict(fitMulti,test)
+    sum(abs(test$Y-yprime)^2)/length(yprime) ##MSE
+}
+
+# Error training con lm
+lmMSEtrain <- mean(sapply(1:5,run_lm_fold,nombre,"train"))
+
+# Error training con ls
+lmMSEtest <- mean(sapply(1:5,run_lm_fold,nombre,"test"))
+
+#------------- 5-fold cross-validation KNN todas las variables
+nombre <- "california"
+run_knn_fold <- function(i, x, tt = "test") {
+    file <- paste(x, "-5-", i, "tra.dat", sep="")
+    x_tra <- read.csv(file, comment.char="@", header=FALSE)
+    file <- paste(x, "-5-", i, "tst.dat", sep="")
+    x_tst <- read.csv(file, comment.char="@", header=FALSE)
+    In <- length(names(x_tra)) - 1
+    names(x_tra)[1:In] <- paste ("X", 1:In, sep="")
+    names(x_tra)[In+1] <- "Y"
+    names(x_tst)[1:In] <- paste ("X", 1:In, sep="")
+    names(x_tst)[In+1] <- "Y"
+    if (tt == "train") {
+        test <- x_tra
+    }
+    else {
+        test <- x_tst
+    }
+    fitMulti=kknn(Y~.,x_tra,test)
+    yprime=fitMulti$fitted.values
+    sum(abs(test$Y-yprime)^2)/length(yprime) ##MSE
+}
+
+# Error training con knn
+knnMSEtrain <- mean(sapply(1:5,run_knn_fold,nombre,"train"))
+
+# Error test con knn
+knnMSEtest <- mean(sapply(1:5,run_knn_fold,nombre,"test"))
+
+
+# Comparativa algoritmos
+
+# Errores medios de test
+resultados <- read.csv("regr_test_alumnos.csv")
+tablatst <- cbind(resultados[,2:dim(resultados)[2]])
+colnames(tablatst) <- names(resultados)[2:dim(resultados)[2]]
+rownames(tablatst) <- resultados[,1]
+
+#l Errores medios de entrenamiento
+resultados <- read.csv("regr_train_alumnos.csv")
+tablatra <- cbind(resultados[,2:dim(resultados)[2]])
+colnames(tablatra) <- names(resultados)[2:dim(resultados)[2]]
+rownames(tablatra) <- resultados[,1]
+
+
+# WILCOXON
+##TABLA NORMALIZADA - lm (other) vs knn (ref) para WILCOXON
+
+diffs <- (tablatst[,1] - tablatst[,2]) / tablatst[,1]
+
+wilcoxon_1_2 <- cbind(
+    ifelse(
+        diffs<0,
+        abs(diffs)+0.1,
+        0+0.1
+    ),
+    ifelse(
+        diffs>0,
+        abs(diffs)+0.1,
+        0+0.1
+    )
+)
+
+colnames(wilcoxon_1_2) <- c(
+    colnames(tablatst)[1],
+    colnames(tablatst)[2]
+)
+
+# Como p-value > 0.05, se acepta la hipótesis nula y se puede confirmar que no hay diferencias significativas
+LMvsKNNtst <- wilcox.test(wilcoxon_1_2[,1], wilcoxon_1_2[,2], alternative = "two.sided", paired=TRUE)
+LMvsKNNtst$statistic
+
+# Como p-value < 0.05, se rechaza la hipótesis nula y se puede afirmar que al menos uno de los algoritmos es diferente
+test_friedman <- friedman.test(as.matrix(tablatst))
+test_friedman
+
+# post hoc homd
+# Al hacer el post-hoc con holm se puede apreciar que el método m5 es significativamente diferente a la regresión lineal múltiple y al knn
+tam <- dim(tablatst)
+groups <- rep(1:tam[2], each=tam[1])
+pairwise.wilcox.test(as.matrix(tablatst), groups, p.adjust = "holm", paired = TRUE)
